eb8c65a24e58f034e98673e934ba7a03e65d92e3
WIlliam Gerenutti <williamgerenuttidm@gmail.com>
2024-02-02 20:34:55 -0300

    :wrench: Create migration for product

---
+++ database/migrations/2022_02_03_135728_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('category');
+            $table->string('brand');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            $table->timestamps();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
