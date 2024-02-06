99e9e9795e17bbd4de5c1c6bba3c811ded617de2
WIlliam Gerenutti <williamgerenuttidm@gmail.com>
2024-02-06 19:27:59 -0300

    :alien: Create routes

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider and assigned to the "api"
+| middleware group. Enjoy building your API!
 |
 */
+
+// Rotas para manipulação de produtos
+Route::prefix('products')->group(function () {
+    Route::get('/', 'ProductController@index');
+    Route::post('/', 'ProductController@store');
+    Route::get('/{id}', 'ProductController@show');
+    Route::put('/{id}', 'ProductController@update');
+    Route::delete('/{id}', 'ProductController@destroy');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
